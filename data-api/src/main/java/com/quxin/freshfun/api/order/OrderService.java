fa8befdd2a6f30166076da96a008141e8be4a7ee
fanyanlin <1260855435@qq.com>
2016-12-29 11:06:28 +0800

    项目基础配置修改

---
+++ data-api/src/main/java/com/quxin/freshfun/api/order/OrderService.java
@@ -0,0 +1,8 @@
+package com.quxin.freshfun.api.order;
+
+/**
+ * Created by fanyanlin on 2016/12/29.
+ */
+public interface OrderService {
+
+}
---
+++ data-impl/src/main/java/com/quxin/freshfun/impl/order/OrderServiceImpl.java
@@ -0,0 +1,9 @@
+package com.quxin.freshfun.impl.order;
+
+import com.quxin.freshfun.api.order.OrderService;
+
+/**
+ * Created by fanyanlin on 2016/12/29.
+ */
+public class OrderServiceImpl implements OrderService {
+}
