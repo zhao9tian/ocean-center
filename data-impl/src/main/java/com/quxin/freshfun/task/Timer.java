d7bdb67eb5e7ea119588ae45f0c60ea6fdeb8cb3
渠成 <2830224613@qq.com>
2017-01-09 17:09:48 +0800

    定时时间设置为每天3点

--- data-impl/src/main/java/com/quxin/freshfun/task/Timer.java
+++ data-impl/src/main/java/com/quxin/freshfun/task/Timer.java
@@ -1,7 +1,11 @@
 package com.quxin.freshfun.task;
 
+import com.quxin.freshfun.api.goods.GoodsDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * 定时器
@@ -10,12 +14,14 @@ import org.springframework.stereotype.Service;
 @Service("timer")
 public class Timer {
 
-
+    @Autowired
+    private GoodsDataService goodsDataService;
 
 //    @Scheduled(cron="0/10 * * * * ? ")   //ss : mm : HH
-    @Scheduled(cron="0/10 * * * * ? ")   //ss : mm : HH
+    @Scheduled(cron="0 0 3 * * ? ")   //ss : mm : HH
     public void autoImportGoodsData() {
-        //System.out.println("timer");
+        System.out.println(new Date()+"开始跑商品数据");
+        goodsDataService.saveGoodsIndicator();
     }
 
 
